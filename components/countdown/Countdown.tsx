8a335da6b36cf571432abbcd28fda9e450b6c410
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-02-27 19:04:38 -0500

    Fixes on styles of Countdown

--- components/countdown/Countdown.tsx
+++ components/countdown/Countdown.tsx
@@ -1,10 +1,10 @@
 import { FC, useState, useEffect } from "react"
 
-import { Box, Center, Grid, GridItem, Text } from "@chakra-ui/react"
+import { Box, Center, Flex, Grid, GridItem, Spacer, Text } from "@chakra-ui/react"
 
 const Countdown: FC = () => {
   const getTimeLeft = () => {
-    const goalDate = new Date(2021, 3, 3).getTime()
+    const goalDate = new Date(2021, 2, 6).getTime()
 
     const msLeft = goalDate - new Date().getTime()
     const days = msLeft / (1000 * 60 * 60 * 24)
@@ -29,53 +29,48 @@ const Countdown: FC = () => {
   })
 
   return (
-    <Box>
-      <Text fontSize="x-large" fontWeight="medium" textAlign="center" margin="10px">
+    <Flex mt="115px" flexDirection="column" color="white" bgGradient="linear(to-r, #8B1B83, #FFB703)" w={["100%", "100px", "100%", "60%"]} mx={["0px", null, "auto"]} justifyContent="center">
+      <Text mt="25px" fontSize="x-large" fontWeight="medium" textAlign="center">
         Preparate lo bueno esta por venir
       </Text>
-      <Grid templateColumns="repeat(4, 1fr)" templateRows="repeat(2, 1fr)" bg="#bababa" w="850px" h="160px" m="auto">
-        <GridItem>
-          <Text fontSize="70px" align="center" fontWeight="bold">
-            {timeLeft.days}
+      <Flex px={["15px", "15px", "45px"]} w="100%" h={["120px", "220px"]} justifyContent="center" alignItems="center">
+        <Grid my="auto" templateRows="repeat(2, 1fr)" h="auto">
+          <Text fontSize={["3rem", "5rem", "6rem", "7rem"]} textAlign="center" fontWeight="bold">
+            {timeLeft.days} 
           </Text>
-        </GridItem>
-        <GridItem>
-          <Text fontSize="70px" align="center" fontWeight="bold">
-            {timeLeft.hours}
-          </Text>
-        </GridItem>
-        <GridItem>
-          <Text fontSize="70px" align="center" fontWeight="bold">
-            {timeLeft.minutes}
-          </Text>
-        </GridItem>
-        <GridItem>
-          <Text fontSize="70px" align="center" fontWeight="bold">
-            {timeLeft.seconds}
-          </Text>
-        </GridItem>
-        <GridItem cente>
-          <Text fontSize="x-large" align="center" fontWeight="medium">
+          <Text mt="0px" fontSize="medium" textAlign="center" fontWeight="medium">
             Dias
           </Text>
-        </GridItem>
-        <GridItem>
-          <Text fontSize="x-large" align="center" fontWeight="medium">
+        </Grid>
+        <Spacer />
+        <Grid my="auto" templateRows="repeat(2, 1fr)">
+          <Text fontSize={["3rem", "5rem", "6rem", "7rem"]} fontWeight="bold">
+            {timeLeft.hours}
+          </Text>
+          <Text mt="0px" fontSize="medium" textAlign="center" fontWeight="medium">
             Horas
           </Text>
-        </GridItem>
-        <GridItem>
-          <Text fontSize="x-large" align="center" fontWeight="medium">
+        </Grid>
+        <Spacer />
+        <Grid my="auto" templateRows="repeat(2, 1fr)">
+          <Text fontSize={["3rem", "5rem", "6rem", "7rem"]} textAlign="center" fontWeight="bold">
+            {timeLeft.minutes}
+          </Text>
+          <Text mt="0px" fontSize="medium" textAlign="center" fontWeight="medium">
             Minutos
           </Text>
-        </GridItem>
-        <GridItem>
-          <Text fontSize="x-large" align="center" fontWeight="medium">
+        </Grid>
+        <Spacer />
+        <Grid my="auto" templateRows="repeat(2, 1fr)">
+          <Text fontSize={["3rem", "5rem", "6rem", "7rem"]} textAlign="center" fontWeight="bold">
+            {timeLeft.seconds}
+          </Text>
+          <Text mt="0px" fontSize="medium" textAlign="center" fontWeight="medium">
             Segundos
           </Text>
-        </GridItem>
-      </Grid>
-    </Box>
+        </Grid>
+      </Flex>
+    </Flex>
   )
 }
 
