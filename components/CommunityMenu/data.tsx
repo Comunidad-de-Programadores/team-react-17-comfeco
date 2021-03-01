b4ab09b2fb0b0c01d6a809aedc8ba25dd6252765
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-03-01 00:46:38 -0500

    Communitites secction added

---
+++ components/CommunityMenu/data.tsx
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    community: "Comunidad de programadores",
+    photo: "/assets/Communities/Comunidad de programadores.png",
+    link: "https://github.com/Comunidad-de-Programadores",
+  },
+  {
+    community: "Stack Overflow",
+    photo: "/assets/Communities/stackoverflow.png",
+    link: "http://stackoverflow.com/",
+  },
+  {
+    community: "GitHub",
+    photo: "/assets/Communities/github.png",
+    link: "https://github.com/",
+  },
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import { Box } from "@chakra-ui/react"
+import { Box, Divider, Flex } from "@chakra-ui/react"
 
 import Protected from "@/components/protected"
 
@@ -9,6 +9,7 @@ import ContentCreators from "@/components/Carousel/Content Creators"
 import Sponsors from "@/components/Carousel/Sponsors"
 
 import HomepageHead from "@/components/Homepage Head"
+import CommuityMenu from "@/components/CommunityMenu"
 
 const Home: FC = () => {
   return (
@@ -20,6 +21,7 @@ const Home: FC = () => {
         </Head>
         <HomepageHead />
         <ContentCreators />
+        <CommuityMenu />
         <Sponsors />
         <Countdown />
       </Box>
