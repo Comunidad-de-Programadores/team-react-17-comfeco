8dd051906646fccc6d3e68423d3121961db58475
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-03-02 22:18:44 -0500

    UserEvents component created

---
+++ components/manageUser/Sections/Mi perfil/Events/data.tsx
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    photo: "/assets/UserEvents/comfeco.png",
+    name: "Comunity Fest and Code",
+    link: "https://comfeco.com",
+  },
+  {
+    photo: "/assets/UserEvents/comfeco.png",
+    name: "Comunity Fest and Code",
+    link: "https://comfeco.com",
+  },
+  {
+    photo: "/assets/UserEvents/comfeco.png",
+    name: "Comunity Fest and Code",
+    link: "https://comfeco.com",
+  },
+  {
+    photo: "/assets/UserEvents/comfeco.png",
+    name: "Comunity Fest and Code",
+    link: "https://comfeco.com",
+  },
+]
