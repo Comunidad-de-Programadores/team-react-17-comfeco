0e512942dafef4609e88c13748ef06a06546b6ec
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-03-06 15:11:42 -0500

    firebase, new configuration

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,20 +1,29 @@
-import firebase from "firebase"
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/storage"
 
-const config = {
+export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
-
-let fire
 
 if (!firebase.apps.length) {
-  fire = firebase.initializeApp(config)
-} else {
-  fire = firebase.app()
+  firebase.initializeApp(firebaseConfig)
 }
 
-export default fire
+const app = firebase.app()
+const auth = firebase.auth()
+const db = firebase.firestore()
+const now = firebase.firestore.Timestamp.now()
+const storage = firebase.storage()
+
+export { auth, db, now, storage }
+
+console.log(app.name ? "Firebase Mode Activated!" : "Firebase not working :(")
