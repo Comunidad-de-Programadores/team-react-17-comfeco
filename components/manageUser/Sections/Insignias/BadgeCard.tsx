e66a4134362d101e8d5b7fc0ed09fe4269ed77e1
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-03-08 00:05:57 -0500

    bgGradient added

--- components/manageUser/Sections/Insignias/BadgeCard.tsx
+++ components/manageUser/Sections/Insignias/BadgeCard.tsx
@@ -11,26 +11,30 @@ type BadgesCardProps = {
 
 const BadgesCard: FC<BadgesCardProps> = ({ img, badgeName, description, howGetIt }) => {
   return (
-    <Box
-      mx="auto"
-      maxW={["90vw", "350px"]}
-      minW={["90vw", "200px"]}
-      h="max-content"
-      border="1px #d4d4d4 solid"
-      borderRadius="10px"
-      p="15px"
-      textAlign="center"
-    >
-      <Avatar boxSize="110px" src={img} />
-      <Text as="b" d="block" fontSize="2.2rem" my="15px">
-        {badgeName}
-      </Text>
-      <Text>{description}</Text>
-      <CustomDivider my="20px" />
-      <Text as="b" d="block" fontSize="1.5rem" mb="15px">
-        ¿Como ganarla?
-      </Text>
-      <Text>{howGetIt}</Text>
+    <Box p="7px" border="1px black solid" borderRadius="10px" bgGradient="linear(to-r, #8B1B83, #FFB703)">
+      <Box
+        mx="auto"
+        maxW={["90vw", "350px"]}
+        minW={["90vw", "200px"]}
+        h="max-content"
+        border="1px black solid"
+        borderRadius="10px"
+        p="15px"
+        textAlign="center"
+        bgColor="#F7FAFC"
+        shadow="1px 1px 5px gray"
+      >
+        <Avatar boxSize="110px" src={img} />
+        <Text as="b" d="block" fontSize="2.2rem" my="15px">
+          {badgeName}
+        </Text>
+        <Text>{description}</Text>
+        <CustomDivider my="20px" color="black" mx="10px" />
+        <Text as="b" d="block" fontSize="1.5rem" mb="15px">
+          ¿Como ganarla?
+        </Text>
+        <Text>{howGetIt}</Text>
+      </Box>
     </Box>
   )
 }
