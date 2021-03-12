292b78d959f5b5a9e99dd8e4485e00a1b6e16ba4
Carlos <57507795+carlosmca44@users.noreply.github.com>
2021-03-11 23:19:00 -0500

    group images

--- components/manageUser/Sections/Grupos/Groups/data.tsx
+++ components/manageUser/Sections/Grupos/Groups/data.tsx
@@ -1,83 +1,83 @@
 export const data = [
   {
-    img: "",
+    img: "/assets/Groups/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg",
     language: "Typescript",
     name: "Group Name1",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg",
     language: "Typescript",
     name: "Group Name2",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/artem-sapegin-ZMraoOybTLQ-unsplash.jpg",
     language: "JavaScript",
     name: "Group Name3",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/artem-sapegin-ZMraoOybTLQ-unsplash.jpg",
     language: "JavaScript",
     name: "Group Name4",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/artem-sapegin-ZMraoOybTLQ-unsplash.jpg",
     language: "JavaScript",
     name: "Group Name5",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg",
     language: "Typescript",
     name: "Group Name6",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg",
     language: "Typescript",
     name: "Group Name7",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg",
     language: "Typescript",
     name: "Group Name8",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/artem-sapegin-ZMraoOybTLQ-unsplash.jpg",
     language: "JavaScript",
     name: "Group Name9",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/artem-sapegin-ZMraoOybTLQ-unsplash.jpg",
     language: "JavaScript",
     name: "Group Name10",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/artem-sapegin-ZMraoOybTLQ-unsplash.jpg",
     language: "JavaScript",
     name: "Group Name11",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magnaaliqua.",
   },
   {
-    img: "",
+    img: "/assets/Groups/artem-sapegin-ZMraoOybTLQ-unsplash.jpg",
     language: "JavaScript",
     name: "Group Name12",
     description:
